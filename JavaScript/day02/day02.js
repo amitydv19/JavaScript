36e7872667962304fde0e31576a88161f86651d7
Amit Yadav <amitray91029@gmail.com>
2026-04-22 00:58:39 +0530

    Day 01

---
+++ JavaScript/day02/day02.js
@@ -0,0 +1,56 @@
+// Primitives data types: jinko copy krne pe real copy milta hai
+// string, number, boolean, null, undefined, symbol, bigint
+
+// Reference data types: jinko copy krne pe real copy nahi milta hai, reference milta hai
+// object {}, array [], function ()
+
+
+// let x = 10;
+// let y = 20;
+
+// if (x > 5 && y < 25) {
+//     console.log("A");
+// } else {
+//     console.log("B");
+// } 
+// // Output: A
+
+
+
+// let isAdmin = true;
+// let isLoggedIn = false;
+
+// if (isAdmin || isLoggedIn) {
+//     console.log("Access granted");
+// } else {
+//     console.log("Access denied");
+// }
+// // Output: Access granted
+
+
+// let temp = 30;
+// if (temp > 25) {
+//     console.log("Hot");
+// } else {
+//     console.log("Pleasant");
+// }
+// // Output: Hot
+
+
+// let a = 0;
+
+// if (a) {
+//     console.log("Truthy");
+// } else {
+//     console.log("Falsy")
+// }
+// // Output: Falsy
+
+
+// let score = 25;
+// let grade = score >= 90 ? "A" : score >= 80 ? "B" : score >= 60 ? "C" : "Fail";
+
+//     console.log(grade);
+// // Output: Score 25 : Fail
+//     //  Score 85 : B
+//     //  Score 95 : A
