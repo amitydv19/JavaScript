72865331f9bbea96e2eb408a8033f4c381be669d
Amit Yadav <amitray91029@gmail.com>
2026-04-25 23:19:27 +0530

    Day 04

---
+++ day04/day04.js
@@ -0,0 +1,33 @@
+// //Loop
+
+// // 1-100
+
+// // for (let start; end; change)
+
+// for(let i = 1; i <= 100; i++) {
+//     console.log(i);
+// }
+
+// // start
+// // while (end) {
+// //     //code
+// //     change
+// // }
+
+// let i = 1;
+// while (i <= 100) {
+//     console.log(i);
+//     i++;
+// }
+
+//start
+// do {
+//     //code
+//     change
+// } while (end)
+
+// let i = 12;
+// do {
+//     console.log(i);
+//     i++;
+// }while (i <= 2);
---
+++ day04/ques.js
@@ -0,0 +1,7 @@
+for (let i=1; i <= 100; i++) {
+    
+    if (i === 50) {
+        continue;
+    }
+    console.log(i);
+}
